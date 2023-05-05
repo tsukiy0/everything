fe9696fd4623ab3631dd43af60d9d0d9bf3e15fa
tsukiy0 <tsukiy0@users.noreply.github.com>
2023-05-05 14:06:56 +0000

    chore: update eslint-config-custom

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
+  plugins: ["prettier", "simple-import-sort", "unused-imports"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "prettier/prettier": ["error"],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "unused-imports/no-unused-imports": "error",
   },
   parserOptions: {
     babelOptions: {
