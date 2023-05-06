e65527f0f3645ed78f3510c31c54c4f81bd26eb9
tsukiy0 <tsukiy0@users.noreply.github.com>
2023-05-06 12:17:39 +0000

    fix: remove required env var

--- apps/deploy/src/index.ts
+++ apps/deploy/src/index.ts
@@ -6,7 +6,6 @@ import { TestStack } from "./TestStack";
 const Environment = z.object({
   TERRAFORM_ORGANIZATION: z.string(),
   TERRAFORM_WORKSPACE: z.string(),
-  CLOUDFLARE_API_TOKEN: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_REGION: z.string(),
