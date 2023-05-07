f0a633e33155846b536d9786a1d7d2d4185938fb
tsukiy0 <tsukiy0@users.noreply.github.com>
2023-05-07 14:28:14 +0000

    refactor: use singleton

--- apps/deploy/src/lambdas/api/index.ts
+++ apps/deploy/src/lambdas/api/index.ts
@@ -1,19 +1,22 @@
+import { LambdaSingleton } from "@tsukiy0/core-aws";
 import serverlessExpress from "@vendia/serverless-express";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import express from "express";
 
-let serverlessExpressInstance: any;
-
-export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
-  if (serverlessExpressInstance) {
-    return serverlessExpressInstance(event, context);
-  }
-
+const serverlessExpressSingleton = new LambdaSingleton<any>(async () => {
   const app = express();
   app.get("/v1/healthcheck", (_, res) => {
     res.status(200).send("OK");
   });
 
   serverlessExpressInstance = serverlessExpress({ app });
+
+  return serverlessExpressInstance;
+});
+
+let serverlessExpressInstance: any;
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  const serverlessExpressInstance = await serverlessExpressSingleton.get();
   return serverlessExpressInstance(event, context);
 };
