10563efd24e282f72fe060259185993196b61886
tsukiy0 <tsukiy0@users.noreply.github.com>
2023-05-07 14:38:09 +0000

    feat: add SecretStringParameter

--- packages/cdktf/src/aws/index.ts
+++ packages/cdktf/src/aws/index.ts
@@ -1,3 +1,4 @@
 export { JsLambdaFunction } from "./JsLambdaFunction";
 export { LambdaHttpApi } from "./LambdaHttpApi";
 export { LambdaSqsQueue } from "./LambdaSqsQueue";
+export { SecretStringParameter } from "./SecretStringParameter";
