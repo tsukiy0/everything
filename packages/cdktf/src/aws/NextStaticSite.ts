a03e28264552c62e1c81cb5ab99867bd2121a940
tsukiy0 <tsukiy0@users.noreply.github.com>
2023-05-28 02:42:51 +0000

    fix: try again with bucket regional domain name

--- packages/cdktf/src/aws/NextStaticSite.ts
+++ packages/cdktf/src/aws/NextStaticSite.ts
@@ -75,7 +75,7 @@ export class NextStaticSite extends Construct {
         origin: [
           {
             originId: "s3",
-            domainName: bucketWebsiteConfiguration.websiteDomain,
+            domainName: bucket.bucketRegionalDomainName,
           },
         ],
         enabled: true,
